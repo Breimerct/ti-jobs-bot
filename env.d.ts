e796b82ee858ca05fab277b90405ca4d7ef9cdf6
Breimerct98 <breimer.correa@zabud.com.co>
2024-10-10 16:56:20 -0500

    Add environment type declaration and add swagger

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      BOT_TOKEN: string;
+    }
+  }
+  
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NextFunction, Request, Response } from 'express';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 8082;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('TI JOBS BOT')
+    .setDescription('The TI JOBS BOT API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/v1/doc', app, document);
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    if (req.path === '/') {
+      res.redirect('/api/v1/doc');
+    } else {
+      next();
+    }
+  });
+
+  await app
+    .listen(PORT)
+    .then(() => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    })
+    .catch((error) => {
+      console.error(`Error starting server: ${error.message}`);
+    });
 }
 bootstrap();
