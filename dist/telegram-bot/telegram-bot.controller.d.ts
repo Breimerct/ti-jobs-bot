e5d47cf3fafc387d769a465015bf740278a36e1d
Breimerct98 <breimerct@gmail.com>
2024-12-01 00:25:37 -0500

    delete definition and configuration files from the dist folder

--- dist/telegram-bot/telegram-bot.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { TelegramBotService } from './telegram-bot.service';
-export declare class TelegramBotController {
-    private readonly telegramBotService;
-    constructor(telegramBotService: TelegramBotService);
-    sendMessage(message: string): Promise<import("typescript-telegram-bot-api/dist/types").Message>;
-}
--- dist/telegram-bot/telegram-bot.d.ts
+++
@@ -1,6 +0,0 @@
-import { CallbackQuery, Message } from 'typescript-telegram-bot-api/dist/types';
-import { TelegramBot } from 'typescript-telegram-bot-api';
-export declare const handles: (botName: string, telegramBot: TelegramBot) => {
-    onMessage: (message: Message) => Promise<void>;
-    onCallbackQuery: (message: CallbackQuery) => Promise<void>;
-};
